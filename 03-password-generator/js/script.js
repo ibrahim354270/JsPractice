ecf210797d9c19f386a1da67e486cd2252bb1fa9
İbrahim Sağlam <saglam.354270@gmail.com>
2024-02-20 19:09:50 +0300

    20.02.24

--- 03-password-generator/js/script.js
+++ 03-password-generator/js/script.js
@@ -8,6 +8,7 @@ const chkNumbers = document.getElementById("chkNumbers");
 const chkSymbols = document.getElementById("chkSymbols");
 const lblStrength = document.getElementById("lblStrength");
 const btnGenerate = document.getElementById("btnGenerate");
+// EVENT FUNCTION
 btnCopy.addEventListener("click", () => {
     const text = lblPassword.textContent;
     if (!text) return;
@@ -43,8 +44,12 @@ btnGenerate.addEventListener("click", () => {
     const password = generatePassword(passwordParams);
     lblPassword.textContent = password;
     // Strength i olustur
+    const strengthPoint = getStrengthPoint(passwordParams);
+    const strengthText = getStrengthIcons(strengthPoint)
+    lblStrength.innerHTML = strengthText;
     // Sifreyi goster
 });
+// GENERIC FUNCTION
 const generatePassword = (passwordParams) => {
     const { passwordLength, hasUpperCase, hasLowerCase, hasNumber, hasSymbol } =
         passwordParams;
@@ -52,20 +57,32 @@ const generatePassword = (passwordParams) => {
     const lowerCaseLetters = "abcdefghijklmnopqrstuywvz";
     const numbers = "0123456789";
     const symbols = "!@#$%^&*()-_=+{}|:;.,?";
+    let allChars = "";
     let password = "";
+    // Yapian secimlere gore en az 1 tane karakterin secimi garanti altina aliniyor.
     if (hasUpperCase) {
         password += getRandomChar(upperCaseLetters);
+        allChars += upperCaseLetters;
     }
     if (hasLowerCase) {
         password += getRandomChar(lowerCaseLetters);
+        allChars += lowerCaseLetters;
     }
     if (hasNumber) {
         password += getRandomChar(numbers);
+        allChars += numbers;
     }
     if (hasSymbol) {
         password += getRandomChar(symbols);
+        allChars += symbols;
     }
-    return password
+    // Kalan kisim olusturuluyor
+    for (let i = password.length; i < passwordLength; i++) {
+        password += getRandomChar(allChars);
+    }
+    // Uretlen sifreyi rasgele karistiriyoruz
+    password = randomSort(password);
+    return password;
 };
 const getRandomChar = (chars) => {
     // Math.floor( Math.random() * (max-min+1) + min)
@@ -81,4 +98,40 @@ const copyTextToClipboard = async (text) => {
     } catch (error) {
         console.log(error);
     }
+};
+const randomSort = (str) => {
+    return str
+        .split("") // metni diziye cevirir, her bir karakter bir dizi elemani olur
+        .sort((a, b) => Math.random() - 0.5) // rasgele siralama yapar
+        .join(""); //  diziyi tekrar stringe cevirir
+};
+// Kendisine gonderilen sifre bilgilerinden bir zorluk puani olusturur
+const getStrengthPoint = (passwordParams) => {
+    const { passwordLength, hasUpperCase, hasLowerCase, hasNumber, hasSymbol } =
+        passwordParams;
+    const strengthPoint =
+        (Number(hasUpperCase) +
+            Number(hasLowerCase) +
+            Number(hasNumber) +
+            Number(hasSymbol) * 2) *
+        passwordLength;
+    return strengthPoint;
+};
+const getStrengthIcons = (point) => {
+    // 1 - 100
+    // 1 - 30 -> Zayif
+    // 31 - 70 -> Normal
+    // 71 - 100 -> Guclu
+    let strengthText = "";
+    let strengthClass = "weak";
+    for (let i = 0; i < Math.ceil(point / 10); i++) {
+        strengthText += "&#9929";
+    }
+    if(point > 70){
+        strengthClass = "strong";
+    }
+    else if(point>30){
+        strengthClass = "normal"
+    }
+    return `<span class="${strengthClass}">${strengthText}</span>`;
 };
